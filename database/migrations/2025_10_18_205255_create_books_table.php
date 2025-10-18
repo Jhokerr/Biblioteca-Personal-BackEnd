a2bbce9e61fc4d13a30cfce580fa6a2850fff2f0
unknown <Olohim Creaciones@PC-DISENO.ELOHIM>
2025-10-18 16:56:43 -0400

    Agregar migraciones para users y books tables

---
+++ database/migrations/2025_10_18_205255_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author')->nullable();
+            $table->text('description')->nullable();
+            $table->string('genre')->nullable();
+            $table->integer('published_year')->nullable();
+            $table->integer('pages')->nullable();
+            $table->string('cover_url')->nullable();
+            $table->string('reading_status')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+            
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
